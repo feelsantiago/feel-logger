6dd4975a07682bd0df3d09c62d7eecddd31229af
Filipe Santiago <filipe@upplify.com>
2020-07-11 16:57:12 -0300

    remove source maps

--- index.ts
+++ index.ts
@@ -1,13 +1,3 @@
-import { Logger } from './src/logger';
-import { LoggerInstance } from './src/logger-instance';
-
-Logger.init();
-const logger = new LoggerInstance('IndexContext');
-logger.info('test', { name: 'filipe' }, 'test', { test: 'a' });
-
-Logger.info('test', 'metadata');
-Logger.info('test', { context: 'TestContext' });
-Logger.info('sem nada');
-
 export * from './src/logger';
 export * from './src/formats';
+export * from './src/logger-instance';
